fc7ae5b788d53929d9d73f195efacc5aa8144328
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-08-26 20:00:11 -0400

    begin passport implementation 43:09

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const path = require('path')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -20,6 +25,17 @@ if (process.env.NODE_ENV === 'development') {
 // Handlebars configuration
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')))
