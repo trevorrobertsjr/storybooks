7712e8af0a3729ecc4f2f3ada6101acbb42ed1e5
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-08-30 07:01:53 -0400

    add edit story. add view story. need to clean up view story

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 // const MongoStore = require('connect-mongo')(session)
@@ -26,13 +27,39 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+)
+
 // Logging in dev environment
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 // Handlebars configuration
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+    '.hbs',
+    exphbs.engine({
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select
+        },
+        defaultLayout: 'main', extname: '.hbs'
+    }));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -50,6 +77,11 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var for other pages to access logged in user
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 // Static files
 app.use(express.static(path.join(__dirname, 'public')))
 
